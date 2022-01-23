0b86bb7ca34896aa2b348a81101aee34a58ad280
Dragoș Străinu <str.dr4605@gmail.com>
2022-01-23 23:20:48 +0200

    chore: update index.tsx wth info about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,8 @@ function Home({ allPostsData }: Props) {
         <meta property="og:type" content="website" />
         <link rel="canonical" href={`${process.env.SITE_URL}`} />
       </Head>
-      <h1>Dragoș Străinu</h1>
+      <h2>Salut</h2>
+      <p>I am Dragoș (Dragosh), a Frontend Software Engineer, Hackathoner, OSS enthusiast.</p>
       <section>
         <ul className={styles.blogPosts}>
           {allPostsData.map(({ slug, date, title, starred }: PostData) => (
