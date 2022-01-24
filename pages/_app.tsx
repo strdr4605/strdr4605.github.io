e7784b26b38174108506c993a29ee776e87c5958
Dragoș Străinu <str.dr4605@gmail.com>
2022-01-24 13:29:40 +0200

    chore: clean gatsby-plugin-offline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,14 @@ import "../styles/globals.css";
 
 const isProduction = process.env.NODE_ENV === "production";
 
+// Remove workers created by gatsby-plugin-offline
+// https://github.com/gatsbyjs/gatsby/issues/2880#issuecomment-349789919
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  window.navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(r => r.unregister())
+  })
+}
+
 if (isProduction) {
   // Notice how we track pageview when route is changed
   Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
