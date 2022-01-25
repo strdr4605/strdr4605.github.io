514ce9d60e67ec61d81665ed0022b25845ad96c1
Dragoș Străinu <str.dr4605@gmail.com>
2022-01-25 11:46:53 +0200

    feat: add firacode font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,16 @@ import Router from "next/router";
 import "prismjs/themes/prism.css";
 import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
+// import "../styles/firacode.css";
 
 const isProduction = process.env.NODE_ENV === "production";
 
 // Remove workers created by gatsby-plugin-offline
 // https://github.com/gatsbyjs/gatsby/issues/2880#issuecomment-349789919
-if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-  window.navigator.serviceWorker.getRegistrations().then(registrations => {
-    registrations.forEach(r => r.unregister())
-  })
+if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+  window.navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((r) => r.unregister());
+  });
 }
 
 if (isProduction) {
