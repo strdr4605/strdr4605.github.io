53f637a5b465030bae373a018d42dd4d2914807a
Dragoș Străinu <str.dr4605@gmail.com>
2022-04-03 14:20:11 +0300

    feat(postSlug): change post layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import Header from "./Header";
 import styles from "./Layout.module.css";
 
-export default function Layout({ children }: React.PropsWithChildren<{}>) {
+export default function Layout({
+  children,
+  hideHeader
+}: React.PropsWithChildren<{ hideHeader?: boolean }>) {
   return (
     <div className={styles.container}>
-      <Header />
+      {!hideHeader && <Header />}
       {children}
     </div>
   );
