ff4a11739b764cd31b11c1cf725d1657e60c55b7
Dragoș Străinu <str.dr4605@gmail.com>
2022-04-07 14:59:10 +0300

    fix: AnimatedName error

--- components/AnimatedName.tsx
+++ components/AnimatedName.tsx
@@ -19,50 +19,90 @@ export default function AnimatedName() {
         let [c] = chars.splice(i, 1);
         chars.splice(chars.length - 5 + i, 0, c);
         await sleep();
-      nameRef.current.innerText = chars.join("");
+        if (!nameRef.current) {
+          return;
+        }
+        nameRef.current.innerText = chars.join("");
         i--;
       }
       i = 6;
       while (i > 2) {
         chars.splice(i, 1);
         await sleep();
-      nameRef.current.innerText = chars.join("");
+        if (!nameRef.current) {
+          return;
+        }
+        nameRef.current.innerText = chars.join("");
         i--;
       }
       chars[0] = chars[0].toLowerCase();
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
       chars[3] = chars[3].toLowerCase();
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
 
       chars[5] = chars[5].toUpperCase();
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
       chars[6] = chars[6].toUpperCase();
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
       chars[7] = chars[7].toUpperCase();
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
       chars[8] = chars[8].toUpperCase();
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
 
       chars[5] = "4";
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
       chars[6] = "6";
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
       chars[7] = "0";
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
       chars[8] = "5";
       await sleep();
+      if (!nameRef.current) {
+        return;
+      }
       nameRef.current.innerText = chars.join("");
     }
     run();
+
+    return () => {
+      nameRef.current = null;
+    };
   }, []);
   return <span ref={nameRef}>Dragoș Străinu</span>;
 }
