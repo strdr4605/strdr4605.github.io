d58811cac7780f9a1de3c97914ee4cbce240ef57
Dragoș Străinu <str.dr4605@gmail.com>
2022-11-04 11:21:27 +0200

    fix: dateTime

--- components/Date.tsx
+++ components/Date.tsx
@@ -3,7 +3,7 @@ import { formatDate } from "../lib/utils";
 export default function Date({ date }: { date: Date }) {
   return (
     <i>
-      <time datatype={date.toISOString().split("T")[0]}>
+      <time dateTime={date.toISOString().split("T")[0]}>
         {formatDate(date)}
       </time>
     </i>
