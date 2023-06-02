f56791959bace43ef1e99f8b9259302a130d09c0
Dragoș Străinu <str.dr4605@gmail.com>
2023-06-02 16:07:00 +0300

    feat: try umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Router from "next/router";
 import "prismjs/themes/prism.css";
 import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
+import Script from "next/script";
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -27,6 +28,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
+      <Script
+        async
+        src="https://analytics.umami.is/script.js"
+        data-website-id="ffd94eb4-d3a5-4505-bcd4-35c97edc57c8"
+      />
     </>
   );
 }
