f808800d078a459bf34b1d3bccbad52c3829a56b
Dragoș Străinu <str.dr4605@gmail.com>
2023-06-06 15:30:56 +0300

    fix: maybe fix umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,26 +4,18 @@ import "prismjs/themes/prism.css";
 import "../styles/globals.css";
 import Script from "next/script";
 
-MyApp.getStaticProps = async () => {
-  const isProduction = process.env.NODE_ENV === "production";
-  return { props: { isProduction } };
-};
-
-function MyApp(props: AppProps & { isProduction: boolean }) {
-  const { Component, pageProps, isProduction } = props;
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
-      {isProduction && (
-        <Script
-          async
-          src="https://analytics.umami.is/script.js"
-          data-website-id="ffd94eb4-d3a5-4505-bcd4-35c97edc57c8"
-        />
-      )}
+      <Script
+        async
+        src="https://analytics.umami.is/script.js"
+        data-website-id="ffd94eb4-d3a5-4505-bcd4-35c97edc57c8"
+      />
     </>
   );
 }
